ed0efd53aa7b7b42dfcc89118fcc93e8968ad557
Elsi <elsi.ikkala@student.lut.fi>
2022-04-14 11:29:12 +0300

    Päivitetty

--- app/src/main/java/com/example/harkkatyo/MainActivity.java
+++ app/src/main/java/com/example/harkkatyo/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.harkkatyo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
+
+    //Review movie Button, takes you to the review view
+    public void reviewLayoutButton(View view){
+        setContentView(R.layout.rewiev);
+    }
+
+
 }
