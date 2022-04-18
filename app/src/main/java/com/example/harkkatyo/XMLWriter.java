61ee1c46bfa274b345288f7366de76444baa0e93
hanna <hanna.hemio@gmail.com>
2022-04-18 14:11:01 +0300

    updates to XMLWriter

--- app/src/main/java/com/example/harkkatyo/XMLWriter.java
+++ app/src/main/java/com/example/harkkatyo/XMLWriter.java
@@ -1,8 +1,30 @@
 package com.example.harkkatyo;
 
-public class XMLWriter {
+import android.content.Context;
 
-    public void write(){
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Date;
+
+public class XMLWriter {
+    private Context context;
+
+    public void write(String name, Date date, String review, float stars ){
+        OutputStreamWriter osw = null;
+        String info ="<Reviews>\n" +
+                "   <name>" + name + "</name>\n" +
+                "   <date>" + date + "</date>\n" +
+                "   <review" + review + "</review>\n" +
+                "   <stars" + stars + "</stars>\n" +
+                "</Reviews>";
+        try{
+            osw = new OutputStreamWriter(context.openFileOutput("reviews.txt", Context.MODE_PRIVATE));
+            osw.write(info);
+            osw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
 
     }
 }
