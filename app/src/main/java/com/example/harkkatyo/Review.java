f7a339875584a1a806849306874c911e64564334
ellahatakka <ella.hatakka@student.lut.fi>
2022-04-20 12:07:57 +0300

    javat added

--- app/src/main/java/com/example/harkkatyo/Review.java
+++ app/src/main/java/com/example/harkkatyo/Review.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 
 import java.util.Calendar;
 
@@ -15,10 +17,12 @@ public class Review extends AppCompatActivity {
     private DatePickerDialog datePicker;
     private Button dateButton;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
         initDatePicker();
         dateButton = findViewById(R.id.selectDate);
     }
--- app/src/main/java/com/example/harkkatyo/Reviews.java
+++ app/src/main/java/com/example/harkkatyo/Reviews.java
@@ -2,17 +2,22 @@ package com.example.harkkatyo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Reviews extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviews);
+
     }
     public void mainpagelayoutButton(View view){
         setContentView(R.layout.activity_mainpage);
